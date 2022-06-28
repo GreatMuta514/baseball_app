781d77be33e579f07313ebbedb090c988d858ca1
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-06-28 14:54:10 +0900

    トップページとbootstrap5の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// bootstrapの設定
+import "bootstrap";
+import "../stylesheets/application.scss";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
