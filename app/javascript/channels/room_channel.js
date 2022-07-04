f1bbbefce226f3923062d3bcf1653e42ede000c2
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-07-04 17:59:01 +0900

    未入力でチャット投稿不可。

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,18 +1,54 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('test')
-  },
+document.addEventListener('turbolinks:load', () => {
+  window.chatContainer = document.getElementById('chat_container')
+  if (chatContainer === null) {
+    return false
+  };
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  //actioncable全般
+  consumer.subscriptions.create("RoomChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // データを受け取った時に以下を発動する
+      document.getElementById('chat_container').
+      insertAdjacentHTML('beforeend', data['chat'])
+    }
+  });
+    
+    //定数定義
+    const submittingChatContent = document.getElementById('submitting_chat_content');
+    const chatButton = document.getElementById('chat_button');
 
-  received(data) {
-    // データを受け取った時に以下を発動する
-    document.getElementById('chat_index').
-    insertAdjacentHTML('beforeend', data['chat'])
-  }
+    //chatroom開いた時に実行
+    window.scrollToBottom = ()=>{
+      window.scroll(0,document.documentElement.scrollHeight)
+    };
+    window.scrollToBottom();
+   
+
+    const button_activation = () => {
+      if (submittingChatContent.value === ''){
+        chatButton.classList.add('disabled')
+      } else {
+        chatButton.classList.remove('disabled')
+      }
+    };
+    //チャット入力欄に値が入った時
+    submittingChatContent.addEventListener('input', () => {
+      button_activation()
+    });
+
+    chatButton.addEventListener('click', () => {
+      chatButton.classList.add('disabled')
+    });
+
 });
+
