6e823da72c0b9d161f2fb20c0de029bd75e2911d
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-07-11 15:01:24 +0900

    予想機能の基盤を作成

---
+++ app/helpers/game_predicts_helper.rb
@@ -0,0 +1,2 @@
+module GamePredictsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rails.application.routes.draw do
   
+
+
+  resources :pro_game_predicts, only: [:new, :create]
+
+  #予想
+  get 'game_predicts' => 'game_predicts#index'
+  get 'game_predicts/confirmation' => 'game_predicts#confirmation'
+  get 'game_predicts/results' => 'game_predicts#results'
+
   #actioncable
   mount ActionCable.server => '/cable'
   #メール関係
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  #チャットルーム
   resources :chatrooms, only: [:index, :show] do
     resources :chats, only: [:create, :destroy]
   end
+
+  #ログイン
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
   
+  #ユーザー
   resources :users
   get 'static_pages/top'
   root 'static_pages#top'
