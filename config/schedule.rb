848e5bf388c4741a406531cada7079091baf8ddb
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-07-18 13:01:47 +0900

    スクレイピングと予想結果の反映をcronで実行

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,26 @@
-set :job_template, "/bin/zsh -l -c ':job'"
-job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+#setは変数の設定
+#eval次のコマンドを打ってね
+
+#ログの吐き出し場所を設定
 set :output, 'log/crontab.log'
+#cronを実行する環境をrailsに設定
 set :environment, ENV['RAILS_ENV']
+#パスを設定している
+env :PATH, ENV['PATH']
+
+#記事から持ってきた
+set :job_template, "/bin/zsh -l -c ':job'"
+job_type :rake, "eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+
 
 every 1.day, :at => '4:30 am' do
-  rake "pro_game:include_today_game"
+  rake "pro_game:include_today_game", environment: 'development'
 end
 
 every 1.day, :at => '4:40 am' do
-  rake "pro_game:include_yesterday_result"
+  rake "pro_game:include_yesterday_result", environment: 'development'
 end
 
 every 1.day, :at => '4:50 am' do
-  rake "pro_game:reflect_predict_result"
+  rake "pro_game:reflect_predict_result", environment: 'development'
 end
