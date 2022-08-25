d94e79946b62111207ac2c5aa016dde6e51bc207
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-08-25 17:23:52 +0900

    eee

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = {
+  entry: {
+		'../../javascript/packs/application.js': './src/js/index.js',
+	},
   module: {
     rules: [
       {
