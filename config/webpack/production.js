bdd5899de31dedb20152cc536dd2691ec7bcd02d
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-08-27 16:36:38 +0900

    loaderの設定をデフォルトに戻した

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,52 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = Object.assign({}, environment.toWebpackConfig(), {
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif|svg)/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'dirname/[contenthash].[ext]',
-            },
-          },
-        ]
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          // [style-loader](/loaders/style-loader)
-          { loader: 'style-loader' },
-          // [css-loader](/loaders/css-loader)
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              // `postcssOptions` is needed for postcss 8.x;
-              // if you use postcss 7.x skip the key
-              postcssOptions: {
-                // postcss plugins, can be exported to postcss.config.js
-                plugins: function () {
-                  return [
-                    require('autoprefixer')
-                  ];
-                }
-              }
-            }
-          },
-          // [sass-loader](/loaders/sass-loader)
-          { loader: 'sass-loader' }
-        ],
-      }
-    ]
-  }
-})
+module.exports = environment.toWebpackConfig();
