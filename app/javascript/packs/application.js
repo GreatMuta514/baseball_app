ad94a27326b407fcc47ff9f20ca4082431c7e551
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-08-27 10:56:39 +0900

    eee

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,13 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 //= require jquery
 //= require jquery_ujs
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 // bootstrapの設定
+import { Tooltip, Toast, Popover } from "bootstrap";
 import "bootstrap";
 import "../src/application";
 // font-awesomeの設定
