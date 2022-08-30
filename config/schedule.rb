1cc0a8354449d5899a38866483a372d25997d694
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-08-30 12:30:25 +0900

    cronタスクの時間を変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,18 +13,18 @@ set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, "eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 
-every 1.day, :at => '6:41 pm' do
+every 1.day, :at => '8:51 am' do
   rake "pro_game:include_today_game", environment: 'development'
 end
 
-every 1.day, :at => '6:42 pm' do
+every 1.day, :at => '8:53 am' do
   rake "chatroom:create", environment: 'development'
 end
 
-every 1.day, :at => '6:43 pm' do
+every 1.day, :at => '8:55 am' do
   rake "pro_game:include_yesterday_result", environment: 'development'
 end
 
-every 1.day, :at => '6:44 pm' do
+every 1.day, :at => '8:57 am' do
   rake "pro_game:reflect_predict_result", environment: 'development'
 end
