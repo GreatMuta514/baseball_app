3cb305fbdd8620bc266bce546ad95dd56c310ded
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-01 15:12:21 +0900

    予想投稿時のエラーを修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-// bootstrapの設定
-// import { Tooltip, Toast, Popover } from "bootstrap";
-// bootstrapのスタイルシート側の機能を読み込む
-import "bootstrap/dist/css/bootstrap.min.css";
-// bootstrapのjs側の機能を読み込む
-import "bootstrap";
-// スタイルシートの読み込み
-import "../src/application";
-
 // font-awesomeの設定
 import '@fortawesome/fontawesome-free/js/all';
 
@@ -25,20 +16,3 @@ ActiveStorage.start()
 // webpackでimage_pack_tagヘルパーメソッドで画像を表示する
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
-
-// require("bootstrap/dist/js/bootstrap");
-
-// $(function() {
-//   function readURL(input) {
-//       if (input.files && input.files[0]) {
-//       var reader = new FileReader();
-//       reader.onload = function (e) {
-//   $('#profile_preview').attr('src', e.target.result);
-//       }
-//       reader.readAsDataURL(input.files[0]);
-//       }
-//   }
-//   $("#profile").change(function(){
-//       readURL(this);
-//   });
-// });
