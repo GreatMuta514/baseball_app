b798c57e3382451085fbf96537fa638712041fbf
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-09 18:04:28 +0900

    ログインユーザーはトップページにアクセスできないようにした

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,10 @@
 class StaticPagesController < ApplicationController
+  before_action :already_logged_in, only: %i[top]
   def top; end
+  
+  private
+
+  def already_logged_in
+    redirect_to chatrooms_path if current_user
+  end
 end
