fa7f48590a0e4339ad1a5d9d2ad909d8a5612233
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-22 17:57:00 +0900

    メールの送信先を指定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def send_contact_email(contact)
     @contact = contact
-    mail to: ENV['TOMAIL'], subject: 'お問い合わせ'
+    mail to: ENV['GMAIL_ADDRESS'], subject: 'お問い合わせ'
   end
 
 end
