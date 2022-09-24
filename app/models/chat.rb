e055eef1fad18283a8defed6685cf6d4ebb933e8
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-24 14:04:04 +0900

    NGワードのバリデーションを付与

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,7 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
-  validates :body, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 100 }, invalid_words: true
 
   def template
     ApplicationController.renderer.render partial: 'chatrooms/chat', locals: { chat: self }
--- app/models/pro_game_predict_comment.rb
+++ app/models/pro_game_predict_comment.rb
@@ -2,7 +2,7 @@ class ProGamePredictComment < ApplicationRecord
   belongs_to :user
   belongs_to :pro_game
 
-  validates :body, presence: true, length: { maximum: 140 }
+  validates :body, presence: true, length: { maximum: 140 }, invalid_words: true
   validates :user_id, presence: true
   validates :pro_game_id, presence: true
 end
