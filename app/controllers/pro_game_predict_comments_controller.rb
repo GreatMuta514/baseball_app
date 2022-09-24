135e50d65fdc5a3ac9d3fc0711d60ffdecb06da2
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-24 18:18:14 +0900

    予想コメント投稿時に予想スコアが表示される

--- app/controllers/pro_game_predict_comments_controller.rb
+++ app/controllers/pro_game_predict_comments_controller.rb
@@ -2,9 +2,9 @@ class ProGamePredictCommentsController < ApplicationController
   
   def create
     @pro_game_predict_comment = current_user.pro_game_predict_comments.new(pro_game_predict_comment_params)
+    @today_pro_game = ProGame.find(params[:pro_game_id])
     if @pro_game_predict_comment.save
     else
-      @today_pro_game = ProGame.find(params[:pro_game_id])
       @pro_game_predict_comments = @today_pro_game.pro_game_predict_comments.order(created_at: :desc).includes(:user, :pro_game)
       render 'pro_game_predicts/new', danger: t(".danger")
     end
