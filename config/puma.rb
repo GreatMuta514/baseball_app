0c6b8c7aeace96ae84b571f1f4db5c2e4dd51435
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-28 12:01:59 +0900

    puma.rbを戻した

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,6 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-bind "unix:///www/baseball_app/shared/tmp/sockets/puma.sock"
-
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -17,7 +15,7 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port ENV.fetch('PORT') { 3000 }
+port ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
