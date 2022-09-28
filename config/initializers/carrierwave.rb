c48b36a606242cd0743b74d05bcc031a9f8b6f5c
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-28 11:44:40 +0900

    carrierwave.rbの編集

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'baseballapp-packet' # 作成したバケット名を記述
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id], # 環境変数
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key], # 環境変数
       region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
