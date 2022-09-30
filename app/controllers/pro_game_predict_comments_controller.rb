8af386988816922c8c39a443c5088beb9ded361a
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-09-30 18:13:12 +0900

    if文をリファクタリング

--- app/controllers/pro_game_predict_comments_controller.rb
+++ app/controllers/pro_game_predict_comments_controller.rb
@@ -2,8 +2,7 @@ class ProGamePredictCommentsController < ApplicationController
   def create
     @pro_game_predict_comment = current_user.pro_game_predict_comments.new(pro_game_predict_comment_params)
     @today_pro_game = ProGame.find(params[:pro_game_id])
-    if @pro_game_predict_comment.save
-    else
+    unless @pro_game_predict_comment.save
       @pro_game_predict_comments = @today_pro_game.pro_game_predict_comments.order(created_at: :desc).includes(:user, :pro_game)
       render 'pro_game_predicts/new', danger: t('.danger')
     end
