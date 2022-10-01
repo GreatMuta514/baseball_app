5665e9002f0ce82965d1613550fbf387b2930888
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-10-01 12:53:57 +0900

    スケジューラーの時間の変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,18 +12,18 @@ env :PATH, ENV['PATH']
 set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, 'eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
 
-every 1.day, at: '12:55' do
+every 1.day, at: '13:00' do
   rake 'pro_game:include_today_game', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:57' do
+every 1.day, at: '13:02' do
   rake 'chatroom:create', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:58' do
+every 1.day, at: '13:04' do
   rake 'pro_game:include_yesterday_result', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:59' do
+every 1.day, at: '13:06' do
   rake 'pro_game:reflect_predict_result', environment: ENV['RAILS_ENV']
 end
