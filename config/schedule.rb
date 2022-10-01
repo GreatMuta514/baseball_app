0983db947acdd4dcbcf7fc7355c90f31fe49265c
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-10-01 11:04:44 +0900

    時間の変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,18 +12,18 @@ env :PATH, ENV['PATH']
 set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, 'eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
 
-every 1.day, at: '11:05 am' do
+every 1.day, at: '11:10 am' do
   rake 'pro_game:include_today_game', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '11:07 am' do
+every 1.day, at: '11:11 am' do
   rake 'chatroom:create', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '11:09 am' do
+every 1.day, at: '11:12 am' do
   rake 'pro_game:include_yesterday_result', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '11:11 am' do
+every 1.day, at: '11:13 am' do
   rake 'pro_game:reflect_predict_result', environment: ENV['RAILS_ENV']
 end
