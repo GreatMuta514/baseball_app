bac7ee0ebdc96fb84acfdf86d9feeb27b217d284
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-10-05 12:51:30 +0900

    試合開始時間を過ぎていると予想を投稿できないようにした

--- app/controllers/pro_game_predicts_controller.rb
+++ app/controllers/pro_game_predicts_controller.rb
@@ -1,8 +1,9 @@
 class ProGamePredictsController < ApplicationController
   before_action :require_login, only: :create
+  before_action :set_today_pro_game, only: %i[new create]
+  before_action :time_check, only: :create
 
   def new
-    @today_pro_game = ProGame.find(params[:pro_game_id])
     @pro_game_predict_comments = @today_pro_game.pro_game_predict_comments.order(created_at: :desc).includes(:user, :pro_game)
     @current_user_predict = current_user.pro_game_predicts.find_by(pro_game_id: params[:pro_game_id]) if current_user
   end
@@ -15,7 +16,7 @@ class ProGamePredictsController < ApplicationController
     if @pro_game_predict.save
       redirect_to request.referer, success: t('.success')
     else
-      render 'pro_game_predict/new'
+      render 'pro_game_predicts/new'
       flash.now[:danger] = t('.danger')
     end
   end
@@ -28,5 +29,17 @@ class ProGamePredictsController < ApplicationController
 
   def predict_params
     params.permit(:first_base_side_score, :third_base_side_score)
+  end
+
+  def set_today_pro_game
+    @today_pro_game = ProGame.find(params[:pro_game_id])
+  end
+
+  def time_check
+    if @today_pro_game.start_at < Time.current
+      flash.now[:danger] = "試合開始予定時間を過ぎているため投稿できません"
+      render 'pro_game_predicts/new'
+      return false
+    end
   end
 end
