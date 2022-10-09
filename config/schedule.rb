639e9635c89c139549e3c9defcb3d5ceddfb04e0
github GreatMuta514 <github@shiba12345678@icloud.com>
2022-10-09 12:49:59 +0900

    時間の編集

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,18 +13,18 @@ env :PATH, ENV['PATH']
 
 job_type :rake, 'eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
 
-every 1.day, at: '12:40' do
+every 1.day, at: '7:00' do
   rake 'pro_game:include_today_game', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:42' do
+every 1.day, at: '7:02' do
   rake 'chatroom:create', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:44' do
+every 1.day, at: '7:04' do
   rake 'pro_game:include_yesterday_result', environment: ENV['RAILS_ENV']
 end
 
-every 1.day, at: '12:46' do
+every 1.day, at: '7:06' do
   rake 'pro_game:reflect_predict_result', environment: ENV['RAILS_ENV']
 end
